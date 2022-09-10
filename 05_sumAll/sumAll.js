f94621f89095eee3a3fe3ef4fcf503cdc641d01c
Antoal83 <aalonsoespadas@gmail.com>
2022-09-10 23:01:53 +0200

    Make exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
-
-};
+const sumAll = function(valueStart, valueEnd) {
+    if (typeof valueStart == "string" || typeof valueEnd == "string" || valueEnd < 0 || valueStart < 0 || Array.isArray(valueEnd) || Array.isArray(valueStart)) {
+  
+      return "ERROR"
+  
+    } else {
+  
+    if(valueStart > valueEnd){
+  
+      let temporaryStart = valueEnd;
+      let temporaryEnd = valueStart;
+      valueStart = temporaryStart
+      valueEnd = temporaryEnd
+  
+      
+    }
+  
+      
+      let savedNumber = valueStart;
+      while (valueStart < valueEnd) {
+  
+        valueStart++;
+        savedNumber += valueStart;
+     
+  
+      }
+  
+      return savedNumber;
+  
+    }
+  
+  }
+  
 
 // Do not edit below this line
 module.exports = sumAll;
